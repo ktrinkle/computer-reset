2032a25c27aa7261dfc638749bd0c0c658df6802
Kevin Trinkle <trinkle@psa-history.org>
2020-12-16 12:28:40 -0600

    Refactor rules and start move process.

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { DataService } from '../data.service';
-import { Signup, StateList, CityList, TimeslotSmall } from '../data';
-import { FormGroup, FormControl, Validators, FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { Signup, StateList, CityList, TimeslotSmall, openEvent } from '../data';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { map, takeUntil } from 'rxjs/operators';
-import { Subject } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { utcToZonedTime } from 'date-fns-tz';
 
 @Component({
@@ -14,14 +14,13 @@ import { utcToZonedTime } from 'date-fns-tz';
 
 export class EventComponent implements OnInit, OnDestroy {
 
-  events = [];
-  
-  public signUp: Signup = {realname: "", cityNm: "", stateCd: "", eventId: 0, 
-    fbId: this.dataService.userFull.facebookId, 
-    firstNm: this.dataService.userFull.firstName, 
+  public signUp: Signup = {realname: "", cityNm: "", stateCd: "", eventId: 0,
+    fbId: this.dataService.userFull.facebookId,
+    firstNm: this.dataService.userFull.firstName,
     lastNm: this.dataService.userFull.lastName
   };
 
+  public events: TimeslotSmall[];
   public agreeInd: boolean = false;
   public eventForm: FormGroup;
   public states: StateList[];
@@ -30,6 +29,12 @@ export class EventComponent implements OnInit, OnDestroy {
   public submitResult: string;
   public submitProcess: boolean = false;
   public loadStatus: boolean = true;
+  public moveOrSignup: boolean;
+  public signedupSlot: number;
+
+  private openEvent: openEvent;
+
+  eventObs = new Subject<openEvent>();
 
   public agreeClick(): void {
     this.agreeInd = true;
@@ -38,7 +43,7 @@ export class EventComponent implements OnInit, OnDestroy {
   destroy$: Subject<void> = new Subject<void>();
 
   constructor(
-    public dataService: DataService, 
+    public dataService: DataService,
     private formBuilder: FormBuilder
     ) { }
 
@@ -55,20 +60,8 @@ export class EventComponent implements OnInit, OnDestroy {
       this.signUp.eventId = this.dataService.eventIdPass;
       this.dataService.eventIdPass = 0;
 
-      this.loadStatus = false;
-      this.dataService.getEvent(this.dataService.userFull.facebookId).subscribe({next: (data: TimeslotSmall[])=>{
-        this.events = data;
-        this.events.forEach((event, index) => {
-          event.eventStartTms = utcToZonedTime(event.eventStartTms, 'America/Chicago');
-          event.eventEndTms = utcToZonedTime(event.eventEndTms, 'America/Chicago');
-          this.events[index] = event;
-        });
-        
-      },
-      complete: () => {
-        //console.log(this.events);
-        this.loadStatus = true;}});
-  
+      this.loadEvents();
+
       this.dataService.getState().subscribe(
         result => { this.states = result; }
         );
@@ -77,20 +70,20 @@ export class EventComponent implements OnInit, OnDestroy {
       if (this.dataService.userFull.realName != null) {
         this.eventForm.patchValue({realName: this.dataService.userFull.realName});
       }
-      
+
       //default us to Dallas, Tx
       if (this.dataService.userFull.stateCode == null ) {
         this.eventForm.patchValue({stateCd: "TX"});
         this.dataService.getCity("TX").pipe(
-          takeUntil(this.destroy$)).subscribe(result => { 
-            this.cities = result; 
+          takeUntil(this.destroy$)).subscribe(result => {
+            this.cities = result;
           }
         );
       } else {
         this.eventForm.patchValue({stateCd: this.dataService.userFull.stateCode});
         this.dataService.getCity(this.dataService.userFull.stateCode).pipe(
-          takeUntil(this.destroy$)).subscribe(result => { 
-            this.cities = result; 
+          takeUntil(this.destroy$)).subscribe(result => {
+            this.cities = result;
           }
         );
       }
@@ -102,25 +95,49 @@ export class EventComponent implements OnInit, OnDestroy {
       }
 
       this.onChanges();
-      
+
   }
 
   ngOnDestroy() {
-
+    this.eventObs.unsubscribe();
   }
 
   isNoteRow = (index, item) => item.eventNote === null ? false: true;
-  
+
+  loadEvents() {
+    //loads all events from web service and parses based on requirements
+
+    this.loadStatus = false;
+
+    this.eventObs = this.dataService.getOpenEventUser(this.dataService.userFull.facebookId).subscribe({next: (data: openEvent)=>{
+      this.openEvent = data;
+      this.events = data.timeslot;
+      this.moveOrSignup = data.moveFlag;
+      this.signedupSlot = data.signedUpTimeslot ?? -1;
+      this.events.forEach((event, index) => {
+        event.eventStartTms = utcToZonedTime(event.eventStartTms, 'America/Chicago');
+        event.eventEndTms = utcToZonedTime(event.eventEndTms, 'America/Chicago');
+        this.events[index] = event;
+      });
+
+    },
+    complete: () => {
+      //console.log(this.events);
+      console.log(this.moveOrSignup);
+      console.log(this.signedupSlot);
+      this.loadStatus = true;}});
+  }
+
   eventSubmit() {
     //builds out signup object. We already have the first pieces.
     this.submitProcess = true;
-    
+
     if (this.eventForm.value.realName) {
       this.signUp.realname = this.eventForm.value.realName;
     }
 
-    this.signUp.cityNm = this.eventForm.value.cityNm; 
-    this.signUp.stateCd = this.eventForm.value.stateCd; //this is a code now
+    this.signUp.cityNm = this.eventForm.value.cityNm;
+    this.signUp.stateCd = this.eventForm.value.stateCd; //this is a code, not an index
 
     this.signUp.eventId = this.eventForm.value.eventId; //always take from form, assuming we pre-populate
 
@@ -131,15 +148,28 @@ export class EventComponent implements OnInit, OnDestroy {
     } else if (!this.signUp.cityNm || !this.signUp.stateCd ) {
       this.submitResult = "We did not find a city or state selected. Please try again.";
       this.submitProcess = false;
+    } else if (!this.moveOrSignup && this.signedupSlot == -1) {
+      this.submitResult = "You are already signed up for an open slot."
+      this.submitProcess = false;
     } else {
       //all is good, lets fire the web service
-      this.dataService.signupForEvent(this.signUp).subscribe((data => {
+      //determine if move or signup
+      if (!this.moveOrSignup) {
+        this.dataService.signupForEvent(this.signUp).subscribe((data => {
           this.submitResult = data;
           this.submitProcess = false;
       }));
+      } else {
+        //assume a move
+        var res = this.dataService.userMoveSlot(this.signedupSlot, this.signUp.eventId, this.signUp.fbId).toString();
+        this.submitResult = res;
+        this.submitProcess = false;
+      }
     }
+    //get updated info
+    this.eventObs.next();
   }
-  
+
   //onchange handler next to clear status for submitResult
 
   onChanges(): void {
@@ -152,8 +182,8 @@ export class EventComponent implements OnInit, OnDestroy {
   changeCityList(event) {
     //console.log("GetCity");
     this.dataService.getCity(event.value).pipe(
-      takeUntil(this.destroy$)).subscribe(result => { 
-        this.cities = result; 
+      takeUntil(this.destroy$)).subscribe(result => {
+        this.cities = result;
       }
     );
   }
